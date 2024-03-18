ff43fa3060b76a7598b0325902189ee48acfe1b6
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-03-18 16:58:31 +0100

    also small changes

--- Project1/3_isPrime.asm
+++ Project1/3_isPrime.asm
@@ -11,12 +11,14 @@ isPrime ;save registers
         ADD     R6, R6, #-1 ; store R3 (storage)
         STR     R3, R6, #0  ; onto the stack
 ;
+;
 ; check if R0 is 2 (prime) - or is less than 2 (not prime)
         ADD     R0, R0, #-2 ; check if R0
         BRz     YES         ; is equal to 2
         ADD     R0, R0, #0
         BRn     NO
         ADD     R0, R0, #2  ; restore R0
+;
 ;
 ; check mod-2 of R0
         AND     R2, R2, #0  ; store R0 into R2 (modulo will change it)
@@ -33,9 +35,12 @@ isPrime ;save registers
         AND     R0, R0, #0  ; restore R0 from R2
         ADD     R0, R2, #0
 ;
+;
 ; check if prime number
         AND     R1, R1, #0  ; initialize R1 to 3
         ADD     R1, R1, #3
+;
+;
 ; sqrt check
 checkP  AND     R2, R2, #0  ; temp storage for R0
         ADD     R2, R0, #0
@@ -56,6 +61,8 @@ checkP  AND     R2, R2, #0  ; temp storage for R0
         ADD     R0, R2, #0
         AND     R1, R1, #0  ; restore R1 from R3
         ADD     R1, R3, #0
+;
+;
 ; mod check
         ADD     R6, R6, #-1 ; store R7 (ret pointer)
         STR     R7, R6, #0  ; onto the stack
@@ -75,8 +82,10 @@ checkP  AND     R2, R2, #0  ; temp storage for R0
 NO      AND     R0, R0, #0
         BRnzp   RESTORE
 ;
+;
 YES     AND     R0, R0, #0
         ADD     R0, R0, #1
+;
 ;
 RESTORE LDR     R3, R6, #0  ; restore R3
         ADD     R6, R6, #1  ; from the stack
@@ -96,19 +105,21 @@ MUL     ; if R1 is 0, return R1
 ; save registers
         ADD     R6, R6, #-1 ; store R2
         STR     R2, R6, #0  ; onto the stack
-;
         AND     R2, R2, #0  ; load R0 into R2
         ADD     R2, R0, #0
         AND     R0, R0, #0
+;
 ;
 repMUL  ADD     R0, R0, R2
         ADD     R1, R1, #-1
         BRp     repMUL
 ;
+;
 ; restore registers
         LDR     R2, R6, #0  ; restore R2
         ADD     R6, R6, #1  ; from the stack
         RET
+;
 ;
 endMZ   AND     R0, R0, #0
         ADD     R0, R1, #0
