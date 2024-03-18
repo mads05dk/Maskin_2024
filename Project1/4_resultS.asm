04880631b396e908026851d3d13685112314860d
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-03-18 18:08:24 +0100

    4_resultS comments done

--- Project1/4_resultS.asm
+++ Project1/4_resultS.asm
@@ -1,18 +1,18 @@
 ;
 ;   Function to take in 0 or 1 and output whether it is prime
 ;
-;   input: R0 - the number to check
+;   input: R0 to check it
 ;
 ;
-resultS ADD     R0, R0, #0
-        BRz     resSN
-        LEA     R0, resP
-        BRnzp   resSP
-resSN   LEA     R0, resNP
-resSP   TRAP    x22
-        LD      R0, NEWLINE
-        TRAP    x21
-        RET
+resultS ADD     R0, R0, #0   ; Create a variable stored in R0
+        BRz     resSN        ; If input is 0 then go to resSN
+        LEA     R0, resP     ; if input was 1 then set R0 to resP
+        BRnzp   resSP        ; jump to trap22
+resSN   LEA     R0, resNP    ; if R0 was 0 set it to resNP
+resSP   TRAP    x22          ; write out string of ASCII
+        LD      R0, NEWLINE  ; Load R0
+        TRAP    x21          ; Write out the R0 result to console
+        RET                  ; Return from subroutine
 ;
 ;
 NEWLINE .FILL x000A
